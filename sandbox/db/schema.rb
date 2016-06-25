9b544a8936a9edca33fc9548d241c6335144fbbd
Ran <lr_naril@163.com>
2016-06-25 15:02:09 +0800

    modify header

--- sandbox/db/schema.rb
+++ sandbox/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623100441) do
+ActiveRecord::Schema.define(version: 20160623104110) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 255, null: false
