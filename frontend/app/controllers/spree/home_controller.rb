16c1e697a1f1b57f501864a330ff6ddc4aa90ef4
hadwinzhy <hadwinzhy@gmail.com>
2016-06-26 06:05:30 -0400

    fix header bug in other page

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -6,7 +6,6 @@ module Spree
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions)
-      @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
   end
 end
