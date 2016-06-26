6c970f39a6a0897bfd79647e3f3edd4f4a85fa64
hadwinzhy <hadwinzhy@gmail.com>
2016-06-26 09:11:05 -0400

    add float kefu

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -7,6 +7,7 @@ module Spree
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions)
       @header_show_all = true
+      @is_home = true
     end
   end
 end
