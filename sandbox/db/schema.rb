ade25e92b8a210d03baaa63e3a02fe332259a8ac
hadwinzhy <hadwinzhy@gmail.com>
2016-06-29 09:02:23 -0400

    add address for user

--- sandbox/db/schema.rb
+++ sandbox/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626132819) do
+ActiveRecord::Schema.define(version: 20160629124259) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 255, null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20160626132819) do
     t.integer  "country_id",        limit: 4
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.integer  "user_id",           limit: 4
+    t.datetime "deleted_at"
   end
 
   add_index "spree_addresses", ["country_id"], name: "index_spree_addresses_on_country_id", using: :btree
@@ -1105,11 +1107,5 @@ ActiveRecord::Schema.define(version: 20160626132819) do
 
   add_index "spree_zones", ["default_tax"], name: "index_spree_zones_on_default_tax", using: :btree
   add_index "spree_zones", ["kind"], name: "index_spree_zones_on_kind", using: :btree
-
-  create_table "ui_configs", force: :cascade do |t|
-    t.string   "search_suggests", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-  end
 
 end
