adc69bde09667af322d3ef4ab7fc8d30400127b3
hadwinzhy <hadwinzhy@gmail.com>
2016-07-02 08:56:32 -0400

    add some order ui

--- frontend/app/assets/javascripts/spree/frontend.js
+++ frontend/app/assets/javascripts/spree/frontend.js
@@ -1,8 +1,10 @@
 //= require bootstrap-sprockets
 //= require jquery.payment
 //= require jquery.validate/jquery.validate.min
+//= require bootstrap-input-number/bootstrap-number-input
 //= require swiper/swiper.jquery.min
 //= require spree
+//= require spree/frontend/components
 //= require spree/frontend/cart
 //= require spree/frontend/checkout
 //= require spree/frontend/checkout/address
