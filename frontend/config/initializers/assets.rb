5548e3b1d8f25bcd7d098f126ca38839f930be31
Ran <lr_naril@163.com>
2016-07-06 01:05:15 +0800

    change payment ui

--- frontend/config/initializers/assets.rb
+++ frontend/config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( favicon.ico credit_cards/* flags/* )
 Rails.application.config.assets.precompile += %w( home/*/* )
 Rails.application.config.assets.precompile += %w( product/* )
+Rails.application.config.assets.precompile += %w( payment/* )
