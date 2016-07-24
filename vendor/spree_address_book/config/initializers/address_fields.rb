9a65bc5e148ce55825fcd7c904e3b8f224aa1730
Ran <lr_naril@163.com>
2016-07-24 17:48:07 +0800

    modify login/signup/account-address(part) views

--- vendor/spree_address_book/config/initializers/address_fields.rb
+++ vendor/spree_address_book/config/initializers/address_fields.rb
@@ -1,2 +1,3 @@
 # ADDRESS_FIELDS = ["firstname", "lastname", "company", "address1", "address2", "city", "state", "zipcode", "country", "phone"]
-ADDRESS_FIELDS = ["firstname", "company", "address1", "address2", "city", "state", "country", "phone"]
+ADDRESS_FIELDS = ["firstname", "lastname", "address1", "city", "state", "zipcode", "country", "phone"]
+# ADDRESS_FIELDS = ["firstname", "company", "address1", "address2", "city", "state", "country", "phone"]
