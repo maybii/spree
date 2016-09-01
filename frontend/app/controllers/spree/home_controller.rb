619b3b319976c47850cd9b62006b7682b2fba797
hadwinzhy <hadwinzhy@gmail.com>
2016-09-01 09:43:59 -0400

    add 首页内容

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -6,6 +6,7 @@ module Spree
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions)
+      @c_taxon = Spree::Taxonomy.where('')
       @header_show_all = true
       @is_home = true
     end
