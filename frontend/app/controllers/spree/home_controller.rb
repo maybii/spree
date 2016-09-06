3f4589c884575551af4289a2eb1370e9e475c715
hadwinzhy <hadwinzhy@gmail.com>
2016-09-06 03:36:10 -0400

    add floor for homepage

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -6,7 +6,6 @@ module Spree
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions)
-      @c_taxon = Spree::Taxonomy.where('')
       @header_show_all = true
       @is_home = true
     end
