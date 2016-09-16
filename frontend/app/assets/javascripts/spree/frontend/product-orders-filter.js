736f494af810e8e5322853888e7a03e27c31eaf1
Ran <lr_naril@163.com>
2016-09-16 21:16:46 +0800

    adjust border of product-item of home and add check into select option

---
+++ frontend/app/assets/javascripts/spree/frontend/product-orders-filter.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('body').on('click', '.btn--orders .order-class', function() {
+    // $(this).closest('.btn--orders').addClass('active').toggleClass('down').siblings('.btn--orders.active').removeClass('active');
+  });
+})
