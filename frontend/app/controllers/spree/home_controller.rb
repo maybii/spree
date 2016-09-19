415ff8fce9d9e1adb8a25f77b78a34de8c062b20
hadwinzhy <hadwinzhy@gmail.com>
2016-09-19 08:54:32 -0400

    some ui fix

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -6,6 +6,7 @@ module Spree
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions).limit(12)
+      @random_products = Spree::Product.where('').sample(12)
       @best_products = Spree::Product.order("sales_amount DESC").limit(12)
       @header_show_all = true
       @is_home = true
