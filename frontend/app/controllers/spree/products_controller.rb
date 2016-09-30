adbce3c594ad4cd5bbfe5d3f864b256c1aa8b6dd
hadwinzhy <hadwinzhy@gmail.com>
2016-09-30 11:01:27 -0400

    add filter for taxons

--- frontend/app/controllers/spree/products_controller.rb
+++ frontend/app/controllers/spree/products_controller.rb
@@ -14,9 +14,11 @@ module Spree
       order_filters = "#{params[:order]} #{(params[:sort] || 'ASC')}" if params[:order] and params[:order] != 'price'
 
       @searcher = build_searcher(params.merge(include_images: true))
+
       all_products = @searcher.all_products
 
       all_properties = Spree::ProductProperty.joins(:property).where("product_id": all_products.map(&:id))
+
       @all_locations = all_properties.where("spree_properties.name" => "原产国").map(&:value).compact.uniq.sort - ['见包装瓶盖所示']
       @all_brands = all_properties.where("spree_properties.name" => "产品品牌").map(&:value).compact.uniq.sort - ['见包装瓶盖所示']
       @all_categries = all_products.joins(:taxons).where("").pluck("spree_taxons.name").uniq - ['见包装瓶盖所示']
--- frontend/app/controllers/spree/taxons_controller.rb
+++ frontend/app/controllers/spree/taxons_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 module Spree
   class TaxonsController < Spree::StoreController
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
@@ -6,11 +7,34 @@ module Spree
     respond_to :html
 
     def show
+      property_filters = [params[:brand], params[:location], params[:category]].delete_if(&:blank?).uniq
+      order_filters = "#{params[:order]} #{(params[:sort] || 'ASC')}" if params[:order] and params[:order] != 'price'
+
       @taxon = Taxon.friendly.find(params[:id])
       return unless @taxon
 
       @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
-      @products = @searcher.retrieve_products
+
+      all_products = @searcher.all_products
+
+      all_properties = Spree::ProductProperty.joins(:property).where("product_id": all_products.map(&:id))
+
+      @all_locations = all_properties.where("spree_properties.name" => "原产国").map(&:value).compact.uniq.sort - ['见包装瓶盖所示']
+      @all_brands = all_properties.where("spree_properties.name" => "产品品牌").map(&:value).compact.uniq.sort - ['见包装瓶盖所示']
+      @all_categries = all_products.joins(:taxons).where("").pluck("spree_taxons.name").uniq - ['见包装瓶盖所示']
+
+      @products = @searcher.retrieve_products(all_products)
+                  .includes(:possible_promotions)
+                  .order( order_filters )
+
+      if(property_filters.present?)
+        @products = @products.joins(:product_properties).where("spree_product_properties.value"=> property_filters)
+      end
+
+      if(params[:order] == 'price')
+        @products = @products.ascend_by_master_price if params[:sort].upcase == 'ASC'
+        @products = @products.descend_by_master_price if params[:sort].upcase == 'DESC'
+      end
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
 
