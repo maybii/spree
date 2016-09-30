6c9bdc3b6087c3922c32a4370e7f5b6488201c78
hadwinzhy <hadwinzhy@gmail.com>
2016-09-30 11:32:35 -0400

    fix cart input change bug

--- frontend/app/assets/javascripts/spree/frontend/cart-details.js
+++ frontend/app/assets/javascripts/spree/frontend/cart-details.js
@@ -10,28 +10,49 @@ $(document).ready(function(){
       $('#checkout-link').removeAttr('disabled').addClass('active');
     }
     var activeItems = _.map($('#cart-detail .line-item.active'), function(item) {
-      return $(item).attr('key')
-    })
-    $('#active_line_id').val(activeItems)
-  })
+      return $(item).attr('key');
+    });
+    $('#active_line_id').val(activeItems);
+  });
 
 
 
   $('.cart-item-checkbox input').on('click', function() {
     if($(this).prop('checked') || $(this).attr('checked')) {
-      $(this).closest('.line-item').addClass('active')
+      $(this).closest('.line-item').addClass('active');
     }else {
-      $(this).closest('.line-item').removeClass('active')
+      $(this).closest('.line-item').removeClass('active');
     }
-    var hasCheckedInput = _.some(_.map($('#cart-detail .cart-item-checkbox input'), function(input) {return $(input).prop('checked')}))
+    var hasCheckedInput = _.some(_.map($('#cart-detail .cart-item-checkbox input'), function(input) {
+      return $(input).prop('checked');
+    }));
     if(hasCheckedInput) {
       $('#checkout-link').removeAttr('disabled').addClass('active');
     }else {
       $('#checkout-link').attr('disabled', 'true').removeClass('active');
     }
     var activeItems = _.map($('#cart-detail .line-item.active'), function(item) {
-      return $(item).attr('key')
-    })
-    $('#active_line_id').val(activeItems)
-  })
-})
+      return $(item).attr('key');
+    });
+    $('#active_line_id').val(activeItems);
+  });
+
+  $('#cart-detail .line_item_quantity').change( function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: $('#update-cart').attr('action'),
+      data: $('#update-cart').serialize(),
+      type: $('#update-cart').attr('method'),
+      success: function(result){
+
+      },
+      error: function(result){
+        /* $(".shipment_total_cost").html("￥" + $shipment_price.toFixed(2));
+         * var orderItemTotalPrice = parseFloat($('#order-item-total')[0].attributes['itemTotal'].value);
+         * $("#summary-order-total").html("￥" + ($shipment_price + orderItemTotalPrice).toFixed(2));*/
+      }
+    });
+    console.log(e);
+  });
+
+});
