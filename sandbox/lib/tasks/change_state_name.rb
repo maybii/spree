2d325a29785c7650f8585e34d9b0aadff99995e8
hadwinzhy <hadwinzhy@gmail.com>
2016-09-30 10:02:49 -0400

    fix some bug change state

--- sandbox/lib/tasks/change_state_name.rb
+++ sandbox/lib/tasks/change_state_name.rb
@@ -6,8 +6,8 @@ Spree::Config[:default_country_id] = china.id
 
 # delete unused id
 Spree::Country.all.each do |country|
-  next if spree.id == china.id
-  spree.destroy
+  next if country.id == china.id
+  country.destroy
 end
 chinese_name = ["北京", "天津", "河北", "山西", "内蒙古", "辽宁", "吉林", "黑龙江", "上海", "江苏", "浙江", "安徽", "附件", "江西", "山东", "河南", "湖北", "湖南", "广东", "广西", "海南", "重庆", "四川", "贵州", "云南", "西藏", "陕西", "甘肃", "青海", "宁夏", "新疆", "台湾", "香港", "澳门"]
 
@@ -19,6 +19,7 @@ end
 
 english_name.each_with_index do |state_name, index|
   state = Spree::State.find_by("name": state_name)
+  next unless state
   state.name = chinese_name[index]
   state.save()
 end
