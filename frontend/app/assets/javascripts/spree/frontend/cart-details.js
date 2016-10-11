c79a7743043e59e989fc9bff951587b1ee945416
Ran <lr_naril@163.com>
2016-10-11 19:26:50 +0800

    fix bug(hide checkbox of cart)

--- frontend/app/assets/javascripts/spree/frontend/cart-details.js
+++ frontend/app/assets/javascripts/spree/frontend/cart-details.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
     if($('.btn-select-all input[name=cartCheckAll]').prop('checked')) {
       $('.btn-select-all input[name=cartCheckAll]').prop('checked', false);
       $('#cart-detail .cart-item-checkbox input').prop('checked', false).closest('.line-item').removeClass('active');;
-      $('#checkout-link').attr('disabled', 'true').removeClass('active');
+      // $('#checkout-link').attr('disabled', 'true').removeClass('active');
     }else {
       $('.btn-select-all input[name=cartCheckAll]').prop('checked', true);
       $('#cart-detail .cart-item-checkbox input').prop('checked', true).closest('.line-item').addClass('active');
-      $('#checkout-link').removeAttr('disabled').addClass('active');
+      // $('#checkout-link').removeAttr('disabled').addClass('active');
     }
     var activeItems = _.map($('#cart-detail .line-item.active'), function(item) {
       return $(item).attr('key');
