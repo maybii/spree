e922f0b9318682f325af445b463aedbd558e9e5d
hadwinzhy <hadwinzhy@gmail.com>
2016-10-14 00:14:32 -0400

    change name

--- sandbox/lib/tasks/config_spree.rb
+++ sandbox/lib/tasks/config_spree.rb
@@ -44,5 +44,5 @@ alipay.save
 
 check = Spree::PaymentMethod::Check.find_or_initialize_by(name: '账户转账')
 check.active = true
-check.description = "<p>开卡行：农业银行内蒙古分行   二连浩特支行</p><p>农业银行卡号： 6228 4550 5601 0194 762  户名：明干巴雅尔</p>"
+check.description = "<p>开户行：中国农业银行</p><p>农业银行卡号： 62284 5505 60101 94762  户名：明干巴雅尔</p>"
 check.save
